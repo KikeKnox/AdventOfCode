d3305c3ec78a184cce0ca8aa49072c92d7c291dd
Aique <kikeknoxville@gmail.com>
2024-01-05 23:49:42 +0100

    Finalizado ejercicio 08

--- 2023/08/08.cpp
+++ 2023/08/08.cpp
@@ -3,25 +3,22 @@
 #include <cstdint>
 #include <fstream>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
 
 using std::string;
 using std::cout;
 using std::vector;
 
-
-
-
 int main(){
     std::ifstream entrada("input.txt");
-    std::ofstream salida("output.txt");
     string linea, instrucciones, mapaActual;
     std::unordered_map<string, vector<string>> mapa;
-    uint16_t inst{0};
+    uint8_t idMaximo{0};
+    uint16_t inst{0}, maximo{0};
     uint64_t pasos{0};
     vector<string> mapaP2;
-    //vector<uint16_t> pasZ;
+    vector<uint16_t> pasZ;
+    
     bool flgPrimeraLinea{true}, flgDerecha, flgAcabaZ{false};
 
     while(getline(entrada, linea)){
@@ -33,33 +30,45 @@ int main(){
         mapa[linea.substr(0,3)] = {linea.substr(7,3),linea.substr(12,3)};
         if('A' == linea[2]) mapaP2.push_back(linea.substr(0,3));
     }
-    mapaActual = mapaP2[0];
-    do{
+    mapaActual = "AAA";
+    while(mapaActual != "ZZZ"){
         if(instrucciones.size() == inst) inst = 0;
         flgDerecha = instrucciones[inst] == 'R';
         mapaActual = mapa[mapaActual][flgDerecha];
         pasos++;
         inst++;
-        if(mapaActual[2] == 'Z' || mapaActual[2] == 'A'){
-            salida<<pasos<<" -> "<<mapaActual<<"\n";
-        }
-    }while(mapaActual != mapaP2[0] && UINT64_MAX != pasos);
-
+    }
     cout<<"Pasos necesarios para ZZZ: "<<pasos<<"\n";
-    // pasos = 0;
-    // inst = 0;
-    // while(!flgAcabaZ && UINT64_MAX != pasos){
-    //     flgAcabaZ = true;
-    //     if(instrucciones.size() == inst) inst = 0;
-    //     flgDerecha = instrucciones[inst] == 'R';
-    //     for(string &mp : mapaP2){
-    //         mp = mapa[mp][flgDerecha];
-    //         if(mp[2] != 'Z') flgAcabaZ = false;
-    //     }
-    //     pasos++;
-    //     inst++;
-    // }
-    // cout<<"Pasos necesarios para todos Z: "<<pasos<<"\n";
+    
+    for(string &s : mapaP2){
+        pasos = 0;
+        inst = 0;
+
+        while(s[2] != 'Z'){
+            if(instrucciones.size() == inst) inst = 0;
+            flgDerecha = instrucciones[inst] == 'R';
+            s = mapa[s][flgDerecha];
+            pasos++;
+            inst++;
+        }
+        pasZ.push_back(pasos);
+        if(maximo < pasos){
+            maximo = pasos;
+            idMaximo = pasZ.size()-1;
+        }
+    }
+    
+    for(uint64_t n{1}; n < UINT64_MAX && !flgAcabaZ; n++){
+        pasos = pasZ[idMaximo] * n;
+        flgAcabaZ = true;
+        for(uint16_t pas : pasZ){
+            if(pasos % pas != 0){
+                flgAcabaZ = false;
+                break;
+            }
+        }
+    }
+    cout<<"Pasos necesarios para todos Z: "<<pasos<<"\n";
 
     return 0;
 }
---
+++ 2023/09/09.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+using std::string;
+using std::cout;
+using std::vector;
+
+int main(){
+    std::ifstream entrada("input.txt");
+    
+
+    return 0;
+}
